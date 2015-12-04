eb81caccfa5b9bede4efd904242520970e028e75
Bert Frees <bertfrees@gmail.com>
2015-12-04 18:37:43 +0100

    Test datatypes and default values

--- clientlib-java-httpclient/src/test/java/org/daisy/pipeline/client/http/WSTest.java
+++ clientlib-java-httpclient/src/test/java/org/daisy/pipeline/client/http/WSTest.java
@@ -7,7 +7,9 @@ import javax.inject.Inject;
 
 import org.daisy.pipeline.client.models.Alive;
 import org.daisy.pipeline.client.models.Argument;
+import org.daisy.pipeline.client.models.datatypes.EnumType;
 import org.daisy.pipeline.client.models.Script;
+import org.daisy.pipeline.client.utils.XML;
 
 import static org.daisy.pipeline.pax.exam.Options.domTraversalPackage;
 import static org.daisy.pipeline.pax.exam.Options.felixDeclarativeServices;
@@ -59,8 +61,76 @@ public class WSTest {
 	public void testScripts() {
 		List<Script> scripts = ws.getScripts();
 		assertEquals(1, scripts.size());
-		Script script = scripts.get(0);
+		Script script = ws.getScript(scripts.get(0).getId());
+		assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+		             "<script xmlns=\"http://www.daisy.org/ns/pipeline/data\" " +
+		                     "href=\"http://localhost:8181/ws/scripts/foo:script\" " +
+		                     "id=\"foo:script\" " +
+		                     "input-filesets=\"daisy202 daisy3\" " +
+		                     "output-filesets=\"epub2 epub3\">\n" +
+		             "<nicename>Example script</nicename>\n" +
+		             "<description>Transforms a Something into a Something.</description>\n" +
+		             "<version>0.0.0-SNAPSHOT</version>\n" +
+		             "<homepage>http://github.com/daisy</homepage>\n" +
+		             "<input desc=\"Input port description.\" " +
+		                    "mediaType=\"application/x-dtbook+xml\" " +
+		                    "name=\"source\" " +
+		                    "nicename=\"Input port\" " +
+		                    "ordered=\"true\" " +
+		                    "required=\"true\" " +
+		                    "sequence=\"false\" " +
+		                    "type=\"anyFileURI\"/>\n" +
+		             "<option data-type=\"foo:choice\" " +
+		                     "desc=\"Enum description.\" " +
+		                     "mediaType=\"\" " +
+		                     "name=\"option-1\" " +
+		                     "nicename=\"Enum\" " +
+		                     "ordered=\"true\" " +
+		                     "required=\"true\" " +
+		                     "sequence=\"false\" " +
+		                     "type=\"string\"/>\n" +
+		             "<option data-type=\"foo:regex\" " +
+		                     "desc=\"Regex description.\" " +
+		                     "mediaType=\"\" " +
+		                     "name=\"option-2\" " +
+		                     "nicename=\"Regex\" " +
+		                     "ordered=\"true\" " +
+		                     "required=\"false\" " +
+		                     "sequence=\"false\" " +
+		                     "type=\"string\"/>\n" +
+		             "<option desc=\"Input HTML.\" " +
+		                     "mediaType=\"application/xhtml+xml " +
+		                     "text/html\" " +
+		                     "name=\"href\" " +
+		                     "nicename=\"HTML\" " +
+		                     "ordered=\"true\" " +
+		                     "required=\"true\" " +
+		                     "sequence=\"false\" " +
+		                     "type=\"anyFileURI\"/>\n" +
+		             "<option desc=\"Whether or not to include or not include something that you may (or may not) want to include.\" " +
+		                     "mediaType=\"\" " +
+		                     "name=\"yes-or-no\" " +
+		                     "nicename=\"Yes? No?\" " +
+		                     "ordered=\"true\" " +
+		                     "required=\"false\" " +
+		                     "sequence=\"false\" " +
+		                     "type=\"boolean\"/>\n" +
+		             "</script>\n",
+		             XML.toString(script.toXml()));
 		assertEquals("Example script", script.getNicename());
+		assertEquals(5, script.getInputs().size());
+		Argument option1 = script.getArgument("option-1");
+		assertTrue(option1.getRequired());
+		assertEquals("foo:choice", option1.getDataType());
+		EnumType choice = (EnumType)ws.getDataType("foo:choice");
+		assertEquals(3, choice.values.size());
+		assertEquals("one", choice.values.get(0).name);
+		assertEquals("two", choice.values.get(1).name);
+		assertEquals("three", choice.values.get(2).name);
+		Argument option2 = script.getArgument("option-2");
+		assertFalse(option2.getRequired());
+		assertEquals("one", option2.getDefaultValue());
+		assertEquals("foo:regex", option2.getDataType().toString());
 	}
 	
 	@Configuration
@@ -80,6 +150,7 @@ public class WSTest {
 				mavenBundle("org.daisy.pipeline:modules-registry:?"), // javax.xml.transform.URIResolver
 				mavenBundle("org.daisy.pipeline:framework-volatile:?"), // org.daisy.pipeline.job.JobStorage
 				mavenBundle("org.daisy.pipeline:woodstox-osgi-adapter:?"), // javax.xml.stream.XMLInputFactory
+				mavenBundle("org.daisy.pipeline:framework-core:?"), // org.daisy.pipeline.datatypes.DatatypeRegistry
 				// FIXME: these belong in the framework as runtime dependencies
 				mavenBundle("commons-io:commons-io:?"),
 				mavenBundle("org.daisy.libs:jing:?")
