2bc75789ea13dc6484aa1b16fa17d0acbfe4a32b
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2016-04-22 18:09:02 +0200

    add support for parsing and estimating progress

--- clientlib-java/src/main/java/org/daisy/pipeline/client/models/Message.java
+++ clientlib-java/src/main/java/org/daisy/pipeline/client/models/Message.java
@@ -12,17 +12,31 @@ public class Message implements Comparable<Message> {
     public String text;
     public Integer line;
     public Integer column;
-    public String timeStamp;
+    public Long timeStamp = new Date().getTime(); // NOTE: the timeStamp is currently not exposed through the web api so we just set it here to the time the object is instantiated instead.
     public String file;
     
-    public Date timeStampDate() {
-    	// TODO: if timeStamp is actually exposed through the web api (I don't think it currently is), then this method might be useful.
-    	return null;
-    }
-
-	@Override
+    @Override
 	public int compareTo(Message other) {
 		return this.sequence - other.sequence;
+	}
+
+	public void setTimeStamp(String timeStampString) {
+		// TODO timeStamp not exposed through the web api yet. Assume UNIX time for now. See: https://github.com/daisy/pipeline-framework/issues/109
+		timeStamp = Long.parseLong(timeStampString);
+	}
+
+	public String formatTimeStamp() {
+		// TODO timeStamp not exposed through the web api yet. Assume UNIX time for now. See: https://github.com/daisy/pipeline-framework/issues/109
+		return ""+timeStamp;
+	}
+	
+	public String getText() {
+		String text = this.text;
+		if (text != null) {
+			// remove progress info from message
+			text = text.replaceAll("^\\[[Pp][Rr][Oo][Gg][Rr][Ee][Ss][Ss][^\\]]*\\] *", "");
+		}
+		return text;
 	}
 
 }
